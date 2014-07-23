ce844ac289af889850526abdd074c6198b9cce50
Alfonso <alfonso@agarzola.com>
2014-07-23 12:07:11 -0400

    Don’t rename _files to .dotfiles

--- slushfile.js
+++ slushfile.js
@@ -74,11 +74,11 @@ gulp.task('default', function (done) {
             answers.appNameSlug = _.slugify(answers.appName);
             gulp.src([__dirname + '/templates/**', __dirname + '/templates/.*'])
                 .pipe(template(answers))
-                .pipe(rename(function (file) {
-                    if (file.basename[0] === '_') {
-                        file.basename = '.' + file.basename.slice(1);
-                    }
-                }))
+                // .pipe(rename(function (file) {
+                //     if (file.basename[0] === '_') {
+                //         file.basename = '.' + file.basename.slice(1);
+                //     }
+                // }))
                 .pipe(conflict('./'))
                 .pipe(gulp.dest('./'))
                 .pipe(install())
